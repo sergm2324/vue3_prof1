e75e40ae5fbd94dc2aae07a7b888b2edd19c978b
Сергей <sergej@Mac-mini-Sergej.local>
2021-02-02 21:40:37 +0300

    исходные файлы добавил

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,17 +1,48 @@
 <template>
   <app-page back title="Корзина">
+    <h3 class="text-center">В корзине пока ничего нет</h3>
+    <table class="table">
+      <thead>
+      <tr>
+        <th>Наименование</th>
+        <th>Количество</th>
+        <th>Цена (шт)</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Название товара</td>
+        <td>
+          <button class="btn primary">+</button>
+          42 шт.
+          <button class="btn danger">-</button>
+        </td>
+        <td>42 000 руб.</td>
+      </tr>
+      </tbody>
+    </table>
+    <hr>
+    <p class="text-right"><strong>Всего: 14 200 руб.</strong></p>
+    <p class="text-right">
+      <button class="btn">Оплатить</button>
+    </p>
   </app-page>
 </template>
 
-
 <script>
 import AppPage from '../components/ui/AppPage'
+const CART_MODEL = {
+  '2': 3,
+  '5': 1
+}
 
 export default {
   setup() {
-
-    return {}
   },
   components: {AppPage}
 }
 </script>
+
+<style scoped>
+
+</style>
