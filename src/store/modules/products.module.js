d12c3913ac9487db06462b6c62ec408ee40232ef
Сергей <sergej@Mac-mini-Sergej.local>
2021-02-02 23:30:57 +0300

    загрузка товаров

--- src/store/modules/products.module.js
+++ src/store/modules/products.module.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import {error} from '../../utils/error'
-const TOKEN_KEY = 'jwt-token'
 
 export default {
     namespaced: true,
@@ -10,12 +9,29 @@ export default {
         }
     },
     mutations: {
+        loadProducts(state, payload) {
+            state.products = payload
+        },
 
     },
     actions: {
-
+        async loadAllProducts({ commit, dispatch }) {
+            try {
+                const url = 'http://localhost:3000/products'
+                const {data} = await axios.get(url)
+                commit('loadProducts', data)
+            } catch (e) {
+                dispatch('setMessage', {
+                    value: error(e.response.data.error.message),
+                    type: 'danger'
+                }, {root: true})
+                throw new Error()
+            }
+        }
     },
     getters: {
-
+        getProducts(state) {
+            return state.products
+        },
     }
 }
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,11 +1,15 @@
 <template>
   <app-loader v-if="loading" />
   <app-page title="Shop" v-else>
+    <button class="btn" @click="loadProducts">Загрузить</button>
+    <div v-for="product in products" :key="product.id">
+      {{product.title}}
+    </div>
   </app-page>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 import {useStore} from 'vuex'
 import AppPage from '../components/ui/AppPage'
 import AppLoader from '../components/ui/AppLoader'
@@ -14,10 +18,10 @@ export default {
   setup() {
     const store = useStore()
     const loading = ref(false)
-
-
     return {
-      loading
+      loading,
+      loadProducts: () => {store.dispatch('products/loadAllProducts')},
+      products: computed(() => store.getters['products/getProducts'])
     }
   },
   components: {
