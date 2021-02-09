260b0e2b37f7b3d08dffd59d92340432e478ae1a
sergey2324 <sergm2324@gmail.com>
2021-02-09 12:30:10 +0300

    выбор количества товаров

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -8,8 +8,8 @@
       </router-link>
       <h4 class="product-title">{{product.title}}</h4>
       <div class="text-center" v-if="product.count > 0">
-        <button class="btn" @click="selectProduct(product.id)" v-if="currentProductId !== product.id">{{ currency(product.price) }}</button>
-        <div class="product-controls" v-if="showProductControls && currentProductId === product.id">
+        <button class="btn" @click="selectProduct(product.id)" v-if="!current.find(e => e.id === product.id)">{{ currency(product.price) }}</button>
+        <div class="product-controls" v-if="current.find(e => e.id === product.id)">
           <button class="btn danger">-</button>
           <strong>123</strong>
           <button class="btn primary">+</button>
@@ -26,7 +26,7 @@
 </template>
 
 <script>
-import {ref, watch} from "vue"
+import {computed, reactive, ref, watch} from "vue"
 import {currency} from '@/utils/currency'
 
 export default {
@@ -34,18 +34,15 @@ name: "ProductsList",
 props: ['products'],
 emits: [],
   setup(_, {emit}) {
-    let showProductControls = ref(false)
-    let currentProductId = ref(null)
+    let current = reactive([])
     const selectProduct = function (productId) {
-      showProductControls.value = true
-      currentProductId.value = productId
+      current.push({id: productId})
     }
 
     return {
       currency,
-      showProductControls,
-      currentProductId,
-      selectProduct
+      selectProduct,
+      current,
     }
   }
 }
